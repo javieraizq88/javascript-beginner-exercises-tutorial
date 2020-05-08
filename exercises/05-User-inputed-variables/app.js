7d457a8e6d51fcfca506bc330ee2258c304158a0
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-05-08 17:18:58 -0400

    06

--- exercises/05-User-inputed-variables/app.js
+++ exercises/05-User-inputed-variables/app.js
@@ -3,4 +3,5 @@ var age = prompt('What is your age?');
 // Your code below:
 var age2 = parseInt(age);
 
+
 console.log(age2+10);
--- exercises/06-Constants/app.js
+++ exercises/06-Constants/app.js
@@ -4,4 +4,5 @@ const VERSION = '0.1';
 
 VERSION = '0.9';
 
+
 console.log(VERSION);
